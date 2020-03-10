bfed91e67b7dcdb16b958c961b3bb646bfacc621
Ramilia Imankulova <ramiliaimankulova@Ramilias-MacBook-Pro.local>
2020-03-10 07:07:48 +0600

    add footer

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
-const links = document.querySelectorAll(".nav-links li");
+const links = document.querySelectorAll(".nav-link");
 
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("open");
